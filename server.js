cc0ee737ef42b6652c909c3faa4141825606b535
Garman_K <garnetbirthstone@hotmail.com>
2020-11-24 22:33:08 -0500

    changes

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+console.log('Connecting to MONGO...', process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useFindAndModify: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
